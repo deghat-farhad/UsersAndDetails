1c998af0c86f887607bd1312cfdd456f52fd7592
smart8bits <smart8bits@gmail.com>
2020-06-04 13:20:07 +0430

    login UI completed.

--- app/src/main/java/com/deghat/farhad/usersanddetails/login/FragLogin.kt
+++ app/src/main/java/com/deghat/farhad/usersanddetails/login/FragLogin.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.deghat.farhad.usersanddetails.R
+import com.deghat.farhad.usersanddetails.di.DaggerViewModelComponent
 import com.deghat.farhad.usersanddetails.di.ViewModelFactory
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.frag_login.view.*
 import javax.inject.Inject
 
@@ -17,19 +24,20 @@ class FragLogin : Fragment() {
 
     private lateinit var viewModel: ViewModelLogin
 
+    private lateinit var btnLogin: Button
+    private lateinit var txtInputEdtTxtEmail: TextInputEditText
+    private lateinit var txtInputEdtTxtPassword: TextInputEditText
+    private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(
+        return inflater.inflate(
             R.layout.frag_login,
             container, false
         )
-
-        view.btnLogin.setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_fragLogin_to_fragUsersList)}
-
-        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,13 +45,66 @@ class FragLogin : Fragment() {
 
         initiate(view)
         injectThisToDagger()
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(ViewModelLogin::class.java)
+        setObservers()
+        setActions()
+    }
+
+    private fun setActions() {
+        btnLogin.setOnClickListener {
+            viewModel.loginBtnTaped(
+                txtInputEdtTxtEmail.text.toString(),
+                txtInputEdtTxtPassword.text.toString()
+            )
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.goToUsersList.observe(
+            this,
+            Observer {
+                navController.navigate(R.id.action_fragLogin_to_fragUsersList)
+            })
+
+        viewModel.loginFailed.observe(
+            this,
+            Observer {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        viewModel.loginIsInProgress.observe(
+            this,
+            Observer {
+                waitForLoggingIn(it)
+            }
+        )
     }
 
     private fun injectThisToDagger() {
-
+        DaggerViewModelComponent
+            .create()
+            .injectFragment(this)
     }
 
     private fun initiate(view: View) {
+        btnLogin = view.btnLogin
+        txtInputEdtTxtEmail = view.txtInputEdtTxtEmail
+        txtInputEdtTxtPassword = view.txtInputEdtTxtPassword
+        navController = Navigation.findNavController(view)
+    }
 
+    private fun waitForLoggingIn(flag: Boolean) {
+        if (flag) {
+            txtInputEdtTxtEmail.isEnabled = false
+            txtInputEdtTxtPassword.isEnabled = false
+            btnLogin.text = getString(R.string.Logging_in)
+            btnLogin.isEnabled = false
+        } else {
+            txtInputEdtTxtEmail.isEnabled = true
+            txtInputEdtTxtPassword.isEnabled = true
+            btnLogin.text = getString(R.string.btnLogin)
+            btnLogin.isEnabled = true
+        }
     }
 }
--- app/src/main/java/com/deghat/farhad/usersanddetails/login/ViewModelLogin.kt
+++ app/src/main/java/com/deghat/farhad/usersanddetails/login/ViewModelLogin.kt
@@ -1,10 +1,46 @@
 package com.deghat.farhad.usersanddetails.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.deghat.farhad.usersanddetails.domain.model.LoginResponse
+import com.deghat.farhad.usersanddetails.domain.usecase.base.DefaultObserver
 import com.deghat.farhad.usersanddetails.domain.usecase.login.Login
+import com.deghat.farhad.usersanddetails.domain.usecase.login.LoginParams
+import com.deghat.farhad.usersanddetails.utils.SingleLiveEvent
 import javax.inject.Inject
 
 class ViewModelLogin @Inject constructor(
     private val login: Login
 ) : ViewModel() {
+    val goToUsersList: SingleLiveEvent<Unit> by lazy { SingleLiveEvent<Unit>() }
+    val loginFailed: SingleLiveEvent<String> by lazy { SingleLiveEvent<String>() }
+    val loginIsInProgress: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+
+    fun loginBtnTaped(email: String, password: String) {
+        login(email, password)
+    }
+
+    private fun login(email: String, password: String){
+        val loginObserver = object : DefaultObserver<LoginResponse>(){
+            override fun onNext(it: LoginResponse) {
+                super.onNext(it)
+                goToUsersList.call()
+            }
+
+            override fun onError(e: Throwable) {
+                super.onError(e)
+                loginFailed.value = e.message
+                loginIsInProgress.value = false
+            }
+
+            override fun onComplete() {
+                super.onComplete()
+                loginIsInProgress.value = false
+            }
+        }
+
+        val loginParams = LoginParams(email, password)
+        loginIsInProgress.value = true
+        login.execute(loginObserver, loginParams)
+    }
 }
